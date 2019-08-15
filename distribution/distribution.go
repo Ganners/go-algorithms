f6ab2ea10ac11877e4b6cd7259bf474429f8ba19
Mark Gannaway <mark@ganners.co.uk>
2019-08-15 17:57:22 +0100

    misc updates

--- distribution/distribution.go
+++ distribution/distribution.go
@@ -5,15 +5,14 @@ import (
 	"hash/fnv"
 )
 
-// RandomDistribution returns a uniform distribution between 0.0 and
-// 1.0 based on a string input.
+// SampleUniformHash returns a good uniform distribution for incrementing
+// variables given a constant ID. The value returned will be between 0.0 and 1.0
 //
 // The first input should be a constant input, perhaps an experiment ID
 // The second input should be a variable input, perhaps a user ID
-func RandomDistribution(constant, variable string) float64 {
+func SampleUniformHash(constant string, variable string) float64 {
 	fnvHash := fnv.New64a()
 	fnvHash.Write([]byte(constant + variable))
 	littleEndian := binary.LittleEndian.Uint64(fnvHash.Sum(nil))
-
 	return float64(littleEndian) / float64(1<<64-1)
 }
--- distribution/distribution_test.go
+++ distribution/distribution_test.go
@@ -5,23 +5,19 @@ import (
 	"testing"
 )
 
-// Basic test to see if we fill 10 buckets with 20 runs
-func TestRandomDistribution(t *testing.T) {
+func TestSampleUniformHash(t *testing.T) {
 	buckets := make([]int, 10)
 	for i := 0; i < 20; i++ {
-
-		dist := RandomDistribution("Something Constant", "User-"+strconv.Itoa(i))
-		dist2 := RandomDistribution("Something Constant", "User-"+strconv.Itoa(i))
-
+		dist := SampleUniformHash("Something Constant", "User-"+strconv.Itoa(i))
+		dist2 := SampleUniformHash("Something Constant", "User-"+strconv.Itoa(i))
 		bucket := int(dist * 10.0)
 		buckets[bucket]++
 
-		// Dist and dist2 should be the same
 		if dist != dist2 {
 			t.Errorf("Different distributions calculated for the same input")
 		}
 	}
-	// Out of 100, each should have at least 1
+	// each bucket should have at least 1
 	for b, count := range buckets {
 		if count == 0 {
 			t.Errorf("Expected to receive count greater than 0 for bucket %d", b)
