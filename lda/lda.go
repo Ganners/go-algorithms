0872431cf6525a41078fdf0f3e159db3e1bb2d91
Mark Gannaway <mark@ganners.co.uk>
2020-11-11 17:04:31 +0000

    keep trying

--- lda/lda.go
+++ lda/lda.go
@@ -4,7 +4,7 @@ import (
 	"math/rand"
 )
 
-type Corpus2 struct {
+type Corpus struct {
 	wordMatrix  [][]string
 	classMatrix [][]int
 	numClasses  int
@@ -12,34 +12,33 @@ type Corpus2 struct {
 	beta        float64
 }
 
-func (c *Corpus2) GibbsSample() {
+func (c *Corpus) GibbsSample() {
 	for j := range c.wordMatrix {
 		for i := range c.wordMatrix[j] {
 			// get probabilities within current row only
-			localMask := c.FullMask()
-			c.UnmaskRow(localMask, j)
-			c.MaskEntry(localMask, j, i)
-
-			localClassesProba := c.Histogram(localMask)
+			mask := c.FullMask()
+			c.UnmaskRow(mask, j)
+			c.MaskEntry(mask, j, i)
+			localClassesProba := c.Histogram(mask)
 			VecDiv(localClassesProba, VecSum(localClassesProba))
 
-			globalMask := c.FullMask()
-			c.UnmaskWord(globalMask, c.wordMatrix[j][i])
-			c.MaskEntry(globalMask, j, i)
-			globalClassesProba := c.Histogram(globalMask)
+			mask = c.FullMask()
+			c.UnmaskWord(mask, c.wordMatrix[j][i])
+			c.MaskEntry(mask, j, i)
+			globalClassesProba := c.Histogram(mask)
 			VecDiv(globalClassesProba, VecSum(globalClassesProba))
 
 			VecAdd(localClassesProba, c.alpha)
 			VecAdd(globalClassesProba, c.beta)
 
 			classProbas := VecMul(localClassesProba, globalClassesProba)
-
-			c.classMatrix[j][i] = ArgMax(classProbas)
+			// c.classMatrix[j][i] = ArgMax(classProbas)
+			c.classMatrix[j][i] = SampleArg(classProbas)
 		}
 	}
 }
 
-func (c *Corpus2) FullMask() [][]int {
+func (c *Corpus) FullMask() [][]int {
 	mask := make([][]int, len(c.classMatrix))
 	for j := range mask {
 		rowMask := make([]int, len(c.classMatrix[j]))
@@ -51,13 +50,13 @@ func (c *Corpus2) FullMask() [][]int {
 	return mask
 }
 
-func (c *Corpus2) UnmaskRow(mask [][]int, row int) {
+func (c *Corpus) UnmaskRow(mask [][]int, row int) {
 	for i := range mask[row] {
 		mask[row][i] = 1.
 	}
 }
 
-func (c *Corpus2) UnmaskWord(mask [][]int, searchWord string) {
+func (c *Corpus) UnmaskWord(mask [][]int, searchWord string) {
 	for j := range c.wordMatrix {
 		for i, word := range c.wordMatrix[j] {
 			if word == searchWord {
@@ -67,11 +66,11 @@ func (c *Corpus2) UnmaskWord(mask [][]int, searchWord string) {
 	}
 }
 
-func (c *Corpus2) MaskEntry(mask [][]int, j, i int) {
+func (c *Corpus) MaskEntry(mask [][]int, j, i int) {
 	mask[j][i] = 0.
 }
 
-func (c *Corpus2) Histogram(mask [][]int) []float64 {
+func (c *Corpus) Histogram(mask [][]int) []float64 {
 	hist := make([]float64, c.numClasses)
 	for j := range c.classMatrix {
 		for i := range c.classMatrix[j] {
@@ -84,21 +83,33 @@ func (c *Corpus2) Histogram(mask [][]int) []float64 {
 	return hist
 }
 
-func NewCorpus(documents []string, numClasses int) *Corpus2 {
+func (c *Corpus) TopDocumentClasses() []int {
+	documentClasses := make([]int, len(c.wordMatrix))
+	for j := range documentClasses {
+		mask := c.FullMask()
+		c.UnmaskRow(mask, j)
+		hist := c.Histogram(mask)
+		documentClasses[j] = ArgMax(hist)
+	}
+	return documentClasses
+}
+
+func NewCorpus(documents []string, numClasses int) *Corpus {
 	wordMatrix := make([][]string, len(documents))
 	classMatrix := make([][]int, len(documents))
-	for i, document := range documents {
-		wordMatrix[i] = wordsFromDocument(document)
-		docWordClasses := make([]int, len(wordMatrix[i]))
+	for j, document := range documents {
+		wordMatrix[j] = wordsFromDocument(document)
+		docWordClasses := make([]int, len(wordMatrix[j]))
 		for i := range docWordClasses {
 			docWordClasses[i] = rand.Intn(numClasses)
 		}
-		classMatrix[i] = docWordClasses
+		classMatrix[j] = docWordClasses
 	}
-	return &Corpus2{
+	return &Corpus{
 		wordMatrix:  wordMatrix,
 		classMatrix: classMatrix,
-		alpha:       1e-10,
-		beta:        1e-10,
+		numClasses:  numClasses,
+		alpha:       1e-1,
+		beta:        1e-2,
 	}
 }
--- lda/utils.go
+++ lda/utils.go
@@ -1,6 +1,10 @@
 package lda
 
-import "strings"
+import (
+	"math/rand"
+	"regexp"
+	"strings"
+)
 
 func VecSum(a []float64) float64 {
 	sum := 0.
@@ -17,6 +21,9 @@ func VecAdd(a []float64, b float64) {
 }
 
 func VecDiv(a []float64, b float64) {
+	if b == 0 {
+		return
+	}
 	for i := range a {
 		a[i] /= b
 	}
@@ -42,13 +49,209 @@ func ArgMax(a []float64) int {
 	return maxI
 }
 
+func SampleArg(a []float64) int {
+	point := rand.Float64() * VecSum(a)
+	cumulative := 0.0
+	for i, v := range a {
+		cumulative += v
+		if point < cumulative {
+			return i
+		}
+	}
+	return len(a) - 1
+}
+
+var stopwords = map[string]struct{}{
+	"a":          struct{}{},
+	"about":      struct{}{},
+	"above":      struct{}{},
+	"after":      struct{}{},
+	"again":      struct{}{},
+	"against":    struct{}{},
+	"all":        struct{}{},
+	"am":         struct{}{},
+	"an":         struct{}{},
+	"and":        struct{}{},
+	"any":        struct{}{},
+	"are":        struct{}{},
+	"aren't":     struct{}{},
+	"as":         struct{}{},
+	"at":         struct{}{},
+	"be":         struct{}{},
+	"because":    struct{}{},
+	"been":       struct{}{},
+	"before":     struct{}{},
+	"being":      struct{}{},
+	"below":      struct{}{},
+	"between":    struct{}{},
+	"both":       struct{}{},
+	"but":        struct{}{},
+	"by":         struct{}{},
+	"can't":      struct{}{},
+	"cannot":     struct{}{},
+	"could":      struct{}{},
+	"couldn't":   struct{}{},
+	"did":        struct{}{},
+	"didn't":     struct{}{},
+	"do":         struct{}{},
+	"does":       struct{}{},
+	"doesn't":    struct{}{},
+	"doing":      struct{}{},
+	"don't":      struct{}{},
+	"down":       struct{}{},
+	"during":     struct{}{},
+	"each":       struct{}{},
+	"few":        struct{}{},
+	"for":        struct{}{},
+	"from":       struct{}{},
+	"further":    struct{}{},
+	"had":        struct{}{},
+	"hadn't":     struct{}{},
+	"has":        struct{}{},
+	"hasn't":     struct{}{},
+	"have":       struct{}{},
+	"haven't":    struct{}{},
+	"having":     struct{}{},
+	"he":         struct{}{},
+	"he'd":       struct{}{},
+	"he'll":      struct{}{},
+	"he's":       struct{}{},
+	"her":        struct{}{},
+	"here":       struct{}{},
+	"here's":     struct{}{},
+	"hers":       struct{}{},
+	"herself":    struct{}{},
+	"him":        struct{}{},
+	"himself":    struct{}{},
+	"his":        struct{}{},
+	"how":        struct{}{},
+	"how's":      struct{}{},
+	"i":          struct{}{},
+	"i'd":        struct{}{},
+	"i'll":       struct{}{},
+	"i'm":        struct{}{},
+	"i've":       struct{}{},
+	"if":         struct{}{},
+	"in":         struct{}{},
+	"into":       struct{}{},
+	"is":         struct{}{},
+	"isn't":      struct{}{},
+	"it":         struct{}{},
+	"it's":       struct{}{},
+	"its":        struct{}{},
+	"itself":     struct{}{},
+	"let's":      struct{}{},
+	"me":         struct{}{},
+	"more":       struct{}{},
+	"most":       struct{}{},
+	"mustn't":    struct{}{},
+	"my":         struct{}{},
+	"myself":     struct{}{},
+	"no":         struct{}{},
+	"nor":        struct{}{},
+	"not":        struct{}{},
+	"of":         struct{}{},
+	"off":        struct{}{},
+	"on":         struct{}{},
+	"once":       struct{}{},
+	"only":       struct{}{},
+	"or":         struct{}{},
+	"other":      struct{}{},
+	"ought":      struct{}{},
+	"our":        struct{}{},
+	"ours":       struct{}{},
+	"ourselves":  struct{}{},
+	"out":        struct{}{},
+	"over":       struct{}{},
+	"own":        struct{}{},
+	"same":       struct{}{},
+	"shan't":     struct{}{},
+	"she":        struct{}{},
+	"she'd":      struct{}{},
+	"she'll":     struct{}{},
+	"she's":      struct{}{},
+	"should":     struct{}{},
+	"shouldn't":  struct{}{},
+	"so":         struct{}{},
+	"some":       struct{}{},
+	"such":       struct{}{},
+	"than":       struct{}{},
+	"that":       struct{}{},
+	"that's":     struct{}{},
+	"the":        struct{}{},
+	"their":      struct{}{},
+	"theirs":     struct{}{},
+	"them":       struct{}{},
+	"themselves": struct{}{},
+	"then":       struct{}{},
+	"there":      struct{}{},
+	"there's":    struct{}{},
+	"these":      struct{}{},
+	"they":       struct{}{},
+	"they'd":     struct{}{},
+	"they'll":    struct{}{},
+	"they're":    struct{}{},
+	"they've":    struct{}{},
+	"this":       struct{}{},
+	"those":      struct{}{},
+	"through":    struct{}{},
+	"to":         struct{}{},
+	"too":        struct{}{},
+	"under":      struct{}{},
+	"until":      struct{}{},
+	"up":         struct{}{},
+	"very":       struct{}{},
+	"was":        struct{}{},
+	"wasn't":     struct{}{},
+	"we":         struct{}{},
+	"we'd":       struct{}{},
+	"we'll":      struct{}{},
+	"we're":      struct{}{},
+	"we've":      struct{}{},
+	"were":       struct{}{},
+	"weren't":    struct{}{},
+	"what":       struct{}{},
+	"what's":     struct{}{},
+	"when":       struct{}{},
+	"when's":     struct{}{},
+	"where":      struct{}{},
+	"where's":    struct{}{},
+	"which":      struct{}{},
+	"while":      struct{}{},
+	"who":        struct{}{},
+	"who's":      struct{}{},
+	"whom":       struct{}{},
+	"why":        struct{}{},
+	"why's":      struct{}{},
+	"with":       struct{}{},
+	"won't":      struct{}{},
+	"would":      struct{}{},
+	"wouldn't":   struct{}{},
+	"you":        struct{}{},
+	"you'd":      struct{}{},
+	"you'll":     struct{}{},
+	"you're":     struct{}{},
+	"you've":     struct{}{},
+	"your":       struct{}{},
+	"yours":      struct{}{},
+	"yourself":   struct{}{},
+	"yourselves": struct{}{},
+}
+
 func wordsFromDocument(document string) []string {
+	document = document
 	document = strings.ToLower(document)
-	document = strings.ReplaceAll(document, "\n", " ")
+	document = stripPunctuation(document)
 	wordsRaw := strings.Split(document, " ")
 	words := make([]string, 0, len(wordsRaw))
 	for _, word := range wordsRaw {
 		if word == "" || word == " " {
+			continue
+		}
+		if len(word) <= 3 {
+			continue
+		}
+		if _, ok := stopwords[word]; ok {
 			continue
 		}
 		word = strings.TrimSpace(word)
@@ -56,4 +259,9 @@ func wordsFromDocument(document string) []string {
 	}
 
 	return words
+}
+
+func stripPunctuation(in string) string {
+	reg, _ := regexp.Compile("[^a-zA-Z0-9 ]+")
+	return reg.ReplaceAllString(in, " ")
 }
